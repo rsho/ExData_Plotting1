307fb0c9a7cca416f6d46c966c99dcd34663bbcc
rsho <rosa.houben@gmail.com>
2015-03-07 22:24:28 +0100

    cleaned up .R code

--- plot3.R
+++ plot3.R
@@ -1,11 +1,14 @@
+#import data, subset, create useable "timedate" variable
 library(lubridate)
-hpc <- read.csv("~/coursera/ExplDataAnalysis/household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
-hpc48<-hpc[which(hpc$Date=="1/2/2007" | hpc$Date=="2/2/2007"),]
-hpc48$datetime<-dmy_hms(paste(hpc48$Date, hpc48$Time, sep=" "))
+hpc <- read.csv("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
+hpc48 <- hpc[which(hpc$Date=="1/2/2007" | hpc$Date=="2/2/2007"),]
+hpc48$datetime <- dmy_hms(paste(hpc48$Date, hpc48$Time, sep=" "))
+
 #make plotting variables numeric
 hpc48$Sub_metering_1 <- as.numeric(hpc48$Sub_metering_1)
 hpc48$Sub_metering_2 <- as.numeric(hpc48$Sub_metering_2)
 hpc48$Sub_metering_3 <- as.numeric(hpc48$Sub_metering_3)
+
 #plot
 png(filename="plot3.png", width=480, height=480, type="cairo-png")
     plot(hpc48$datetime, hpc48$Sub_metering_1, xlab="", ylab="Energy sub metering", type="n")
--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
 #import data, subset, create useable "timedate" variable
 library(lubridate)
-hpc <- read.csv("~/coursera/ExplDataAnalysis/household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
+hpc <- read.csv("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
 hpc48 <- hpc[which(hpc$Date=="1/2/2007" | hpc$Date=="2/2/2007"),]
 hpc48$datetime <- dmy_hms(paste(hpc48$Date, hpc48$Time, sep=" "))
 
